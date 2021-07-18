4e91a5a3896c8e55990a285c51803f7f81f5be27
Alex Thomas <alex-thomas92@outlook.com>
2021-07-18 14:31:16 +0100

    card free version styling

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,12 @@
 export default function Card({ name, image, url, free, price, company }) {
   return (
     <div className="border border-indigo-100 rounded-lg shadow-lg">
-      <a href="#" className="block bg-white rounded-lg shadow-sm h-100">
+      <a href={`/${url}`} className="block bg-white rounded-lg shadow-sm h-100">
         <img src={image || "aiteam.svg"} alt="ai" />
       </a>
       <div className="flex items-center justify-between p-1 mt-3">
         <div>
-          <a href={url} className="font-medium">
+          <a href={`/{url}`} className="font-medium">
             {name}
           </a>
           <a className="flex items-center" href={company.url}>
@@ -16,10 +16,12 @@ export default function Card({ name, image, url, free, price, company }) {
             </span>
           </a>
         </div>
-        <span className="flex items-center h-8 px-2 text-sm text-indigo-600 bg-indigo-200 rounded">
-          {price}
-        </span>
-        {free && <span>Free version available</span>}
+        <div className="flex flex-col items-end justify-end">
+          <span className="flex items-center h-8 px-2 text-sm text-indigo-600 bg-indigo-200 rounded">
+            {price}
+          </span>
+          {free && <span className="text-xs">*Free version available</span>}
+        </div>
       </div>
     </div>
   );
