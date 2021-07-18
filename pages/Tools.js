ec6fbd9669254f217440f39582fbf340d43b52f3
Alex Thomas <alex-thomas92@outlook.com>
2021-07-18 14:27:57 +0100

    removed commented out filters

--- pages/Tools.js
+++ pages/Tools.js
@@ -49,62 +49,6 @@ export default function Tools({ tools }) {
             />
           </main>
           <section>
-            {/* <h2 className="text-3xl">Product Category Page Title</h2> */}
-            {/* <div className="flex flex-col mt-6 sm:flex-row sm:items-end sm:justify-between">
-              <span className="text-sm font-semibold">
-                {Math.ceil(page + 1 / limit)}-{Math.ceil(limit / page)} of
-                {total} Tools
-              </span>
-              <button className="relative mt-4 text-sm focus:outline-none group sm:mt-0">
-                <div className="flex items-center justify-between w-40 h-10 px-3 border-2 border-indigo-300 rounded hover:bg-indigo-300">
-                  <span className="font-medium">Popular</span>
-                  <svg
-                    className="w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <div className="absolute z-10 flex-col items-start hidden w-full pb-1 bg-white rounded shadow-lg group-focus:flex">
-                  <a
-                    className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                    href="#"
-                  >
-                    Popular
-                  </a>
-                  <a
-                    className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                    href="#"
-                  >
-                    Featured
-                  </a>
-                  <a
-                    className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                    href="#"
-                  >
-                    Newest
-                  </a>
-                  <a
-                    className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                    href="#"
-                  >
-                    Lowest Price
-                  </a>
-                  <a
-                    className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                    href="#"
-                  >
-                    Highest Price
-                  </a>
-                </div>
-              </button>
-            </div> */}
             <div className="grid w-full grid-cols-1 mt-6 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-x-6 gap-y-12">
               {toolsList.map((tool) => (
                 <Card key={tool.id} {...tool} />
