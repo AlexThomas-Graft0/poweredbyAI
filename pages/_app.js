46d1711b4cc57310dc0e7a32578d95dcdc01e5e9
Alex Thomas <alex-thomas92@outlook.com>
2021-07-22 10:18:11 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
+
 import "../styles/globals.css";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className="bg-indigo-800">
       <Component {...pageProps} />
