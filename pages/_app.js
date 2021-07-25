bd8005c7e98743124dd6b43d92588ceb65c5d63e
Alex Thomas <alex-thomas92@outlook.com>
2021-07-25 14:13:33 +0100

    As for hosting, I include 1 year hosting in the price, then set auto-renewal up on the clients details ls

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,29 @@ import * as ga from "../lib/ga";
 import "../styles/globals.css";
 import Footer from "../components/Footer";
 
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+
+export const event = ({ action, category, label, value }) => {
+  ga.event("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
+    event({
+      action: "siteview",
+      category: "main",
+      label: "test",
+    });
     const handleRouteChange = (url) => {
+      console.log(`changing route to: ${url}`);
       ga.pageview(url);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
